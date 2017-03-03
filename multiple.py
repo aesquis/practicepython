8e16b4fc2183895ebd11794f3ffd81e1d2981e58
aesquis <aesquis@gmail.com>
2017-03-03 10:59:20 +0100

    Create multiple.py

---
+++ multiple.py
@@ -0,0 +1,9 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+
+num1 = int(input("Digues un nombre :"))
+
+if (num1 % 4 == 0):
+    print("%d es multiple de 4" %num1)
+else:
+    print("%d no es multiple de 4" %num1)   
